f986991c9168fb38153035915328d9c4cf99c454
Arvaaz <arvazsaifi17@gmail.com>
2022-03-05 14:26:40 +0530

    Create_object_submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,8 @@
-function CreateObject(arr) {
-    // Write your code here
-}
-
+function CreateObject(arr){
+    d={}
+    for (let i=0;i<((arr.length)-1);i=i+2){
+      d[arr[i]]=arr[i+1]
+      }
+    return d
+  }
 module.exports = CreateObject;
